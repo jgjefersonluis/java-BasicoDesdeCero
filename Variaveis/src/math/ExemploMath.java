a11f143b5515ac0050a182df5d4c0a9b2ff688f8
Jeferson Luis <jgjefersonluis@gmail.com>
2020-04-02 12:19:29 -0300

    Classe Math

---
+++ Variaveis/src/math/ExemploMath.java
@@ -0,0 +1,23 @@
+package math;
+
+public class ExemploMath {
+
+    public static void main(String args[]) {
+        /*
+         int base = 5;
+
+         int expo = 5;
+
+         double res;
+
+         res = Math.pow(base, expo);
+         */
+        int raiz;
+
+        raiz = (int) Math.sqrt(16);
+
+        System.out.println(raiz);
+
+    }
+
+}
