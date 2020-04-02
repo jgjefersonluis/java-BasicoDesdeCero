68f66763cdda354c9a26bf440bcf1b6b71716f65
Jeferson Luis <jgjefersonluis@gmail.com>
2020-04-02 11:41:38 -0300

    Variaveis

---
+++ Variaveis/src/variaveis/ExemploVariaveis.java
@@ -0,0 +1,22 @@
+package variaveis;
+
+public class ExemploVariaveis {
+
+    public static void main(String[] args) {
+      int num = 10;
+
+        int cont = 5;
+
+        // System.out.print(num);
+        // System.out.println(cont);
+        /*
+         int cambio;
+         cambio = num + cont;
+         */
+        System.out.println("En numero que ingressaste es: " + num);
+        System.out.println("El valor con el que cambio tal dato fue: " + cont);
+        //    System.out.println("El nuevo valor: " + cambio);
+        System.out.println("El nuevo valor es: " + num);
+    }
+
+}
